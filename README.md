e8f07ce87f4b22dfd8522ef4cb2a99ba12e630ca
millea <millea1994@gmail.com>
2018-06-01 19:13:55 +0700

    add setup database and crud mongo with express

---
+++ README.md
@@ -0,0 +1,18 @@
+# Project Title
+
+CRUD MongoDB by using API with Express.js
+
+## Description
+
+Setup the mongoose database and configuration with Express.js as well as CRUD by using the API created in Express.js
+
+## Getting Started
+
+### Dependencies
+
+* Install Nodejs
+* Install MongoDB
+* Install Express and Mongoose
+
+
+
---
+++ server.js
@@ -0,0 +1,61 @@
+var express		= require('express');
+var bodyParser	= require('body-parser');
+var app			= express();
+var router		= express.Router();
+var port 		= process.env.PORT || 3000;
+
+// setting database
+var mongoose 	= require('mongoose');
+mongoose.connect('mongodb://localhost/apiusers');
+
+var user  = require('./models/user');
+// body-parser configuration
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+router.get('/', function(req, res){
+	res.json({message: 'welcome to my tutorial'});
+});
+
+router.route('/members')
+	.post(function(req, res){
+		var user 		= new User();
+		user.name		= req.body.name;
+		user.password	= req.body.password;
+		user.save(function(err){
+			if(err) res.send(err);
+			res.json({message: 'user successfully added'});
+		})
+	}).get(function(req, res){
+		User.find(function(err, users){
+			if(err) res.send(err);
+			res.json(users);
+		});
+	});
+
+router.route('/members/:name')
+	.get(function(req, res){
+		User.find({name : req.params.name}, function(err, user){
+			if(err) res.send(err);
+			res.json(user);
+		})
+	}).put(function(req, res){
+		User.update(
+			{ name : req.params.name },
+			{ name : req.body.name}, 
+			function(err){
+				if(err) res.send(err);
+				res.json({message: 'user successfully updated'});
+			});
+	}).delete(function(req, res){
+		User.remove({name : req.params.name}, function(err){
+			if(err) res.send(err);
+			res.json({message: 'user successfully deleted'});
+		});
+	});
+
+// prefix API
+app.use('/api', router);
+
+app.listen(port);
+
